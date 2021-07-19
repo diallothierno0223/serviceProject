063db0413218f9e189b4d655ade95a76b72c8857
thierno <thiernosaliou0223@gmail.com>
2021-07-19 00:11:30 +0000

    add template

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A nisi architecto eos aliquam corrupti et, officiis vero eveniet odit consequuntur, iure unde culpa dolor labore incidunt? Sunt sint ex eum.</p>
+@endsection
